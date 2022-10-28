c3f937526ecab1c687a2ad0f37de188cbf343675
ndhakal <nabindhakall@hotmail.com>
2022-10-28 14:30:05 -0700

    Traial mainsceen gui, AB#868

--- src/main/java/com/lastcruise/Main.java
+++ src/main/java/com/lastcruise/Main.java
@@ -3,6 +3,7 @@ package com.lastcruise;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lastcruise.controller.Controller;
+import com.lastcruise.controller.GUIController;
 import com.lastcruise.model.AllSounds;
 import com.lastcruise.model.Music;
 import java.io.IOException;
@@ -14,21 +15,23 @@ public class Main {
 
   public static void main(String[] args) throws IOException, InterruptedException {
 
-      Controller controller = new Controller();
-      boolean runGame = controller.gameSetUp();
+    GUIController guiController = new GUIController();
 
-      URL backgroundMusic = Main.class.getResource(AllSounds.ALL_SOUNDS.get("main"));
-      Music.runAudio(backgroundMusic);
+    Controller controller = new Controller();
+    boolean runGame = controller.gameSetUp();
 
-      while(runGame){
-        runGame = controller.getCommand();
-        controller.updateView();
-      }
+    URL backgroundMusic = Main.class.getResource(AllSounds.ALL_SOUNDS.get("main"));
+    Music.runAudio(backgroundMusic);
 
-      Music.muteMusic();
+    while (runGame) {
+      runGame = controller.getCommand();
+      controller.updateView();
+    }
 
-      // prevents cmd/terminal window from closing right away after game ends when playing JAR file
-      System.out.println("Thanks for playing! This window will soon close.");
-      Thread.sleep(5000);
+    Music.muteMusic();
+
+    // prevents cmd/terminal window from closing right away after game ends when playing JAR file
+    System.out.println("Thanks for playing! This window will soon close.");
+    Thread.sleep(5000);
   }
 }
